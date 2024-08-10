60654062e003d65517d1593953fc902209d055fb
rohan-kapoor <kapoorrohan2004@gmail.com>
2024-08-10 09:25:16 +0530

    updated codes

--- Web_Dev/frontend/index.js
+++ Web_Dev/frontend/index.js
@@ -7,33 +7,75 @@
 //     {name: "Product 2", description: "Some additional details about P2", price: 200},
 // ]
 
-var productsList = [];
-
-const getproductList = () => {
-    productsList = [
-        {name: "Product 1", description: "Some additional details about P1", price: 100},
-        {name: "Product 2", description: "Some additional details about P2", price: 200},
-    ]
-    initProductList();
-}
-
-const initProductList = () => {
-  document.getElementById("productMenu").innerHTML = `
-    <h3>List of Products</h3>
-    <table>
-            <thead>
-                <tr>
-                    <th>Sr. No.</th>
+function renderProducts() {
+    let productsListFromServer = [];
+  
+    function getProductsList() {
+      productsListFromServer = productsList;
+      initProductsListTable();
+    }
+  
+    function initProductsListTable() {
+      if (!productsListFromServer || productsListFromServer?.length === 0) {
+        document.getElementById("productMenu").innerHTML = `${getLoader()}`;
+        return;
+      }
+      document.getElementById("productMenu").innerHTML = `
+          <div class="row center">
+            <div class="col-6">
+              <table>
+                <thead>
+                  <tr>
+                    <th>Sr. No. </th>
                     <th>Product Name</th>
-                    <th>Details</th>
+                    <th>Description</th>
                     <th>Price</th>
-                </tr>
-            </thead>
-            <tbody>
-            ${getProductListHTMLUsingMap()}
-            </tbody>
-        </table>`;
-};
+                  </tr>
+                </thead>
+                ${getProductListHTMLUsingMap()}
+              </table>
+            </div>
+          </div>
+        `;
+    }
+  
+    function getProductListHTMLUsingMap() {
+      if (!productsListFromServer || productsListFromServer?.length === 0) {
+        return ``;
+      }
+      var rowsOfProducts = productsListFromServer?.map((item, index) => {
+        return `<tr>
+                <td>${index + 1}</td>
+                <td>${item.name}</td>
+                <td>${item.description}</td>
+                <td>${item.price}</td>
+              </tr>`;
+      });
+  
+      return `
+        <tbody>
+          ${rowsOfProducts?.join("")}
+        </tbody>
+      `;
+    }
+  
+    function getLoader() {
+      if (!productsListFromServer || productsListFromServer?.length === 0) {
+        return `
+          <div class="row">
+            <div class="col">
+              Loading Data
+            </div>
+          </div>
+        `;
+      } else return ``;
+    }
+  
+    initProductsListTable();
+    setTimeout(getProductsList, 5000);
+  }
+  
+  renderProducts();
 
 // function getProductListHTML(){
 //     var rows = ""
@@ -48,18 +90,18 @@ const initProductList = () => {
 //     return rows
 // }
 
-function getProductListHTMLUsingMap() {
-    var rowsOfProducts = productsList.map((item,index) => {
-        return `<tr>
-                <td>${index+1}</td>
-                <td>${item.name}</td>
-                <td>${item.description}</td>
-                <td>${item.price}</td>
-            </tr>`;
-    });
-    return rowsOfProducts.join("");  //for removing , as returning array elements
-}
+// function getProductListHTMLUsingMap() {
+//     var rowsOfProducts = productsList.map((item,index) => {
+//         return `<tr>
+//                 <td>${index+1}</td>
+//                 <td>${item.name}</td>
+//                 <td>${item.description}</td>
+//                 <td>${item.price}</td>
+//             </tr>`;
+//     });
+//     return rowsOfProducts.join("");  //for removing , as returning array elements
+// }
 
-// init();
-initProductList();
-setTimeout(getproductList,5000);
+// // init();
+// initProductList();
+// setTimeout(getproductList,5000);
